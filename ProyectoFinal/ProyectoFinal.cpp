5c33bf026eb3bceff0f50d7d4fd38086a0e4066d
Karla Ortiz <gabriela.ortiz@galileo.edu>
2021-05-01 13:04:06 -0600

    First commit

---
+++ ProyectoFinal/ProyectoFinal.cpp
@@ -0,0 +1,12 @@
+// ProyectoFinal.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello World!\n";
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
